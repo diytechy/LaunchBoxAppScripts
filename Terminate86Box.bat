9f2c6b2c7668c6c4db7a0ab53e799510c3706e63
Peter Johnson <peterandersjohnson@outlook.com>
2025-03-05 23:03:02 -0600

    More reference files + change detection update, mouse to follow

---
+++ Terminate86Box.bat
@@ -0,0 +1,2 @@
+cd /d %~dp0
+Powershell.exe -executionpolicy remotesigned -File  .\Terminate86Box.ps1
---
+++ TerminateSS.ps1
@@ -0,0 +1,31 @@
+$winttl="SheepShaver"
+$delaySeconds = 7
+
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class User32 {
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, string lParam);
+}
+"@
+
+$AllProc = Get-Process | Where-Object {$_.MainWindowTitle -like $winttl}
+$PreTerm = $AllProc | Select-Object Id, ProcessName
+if ($AllProc.Count -gt 1) {$SelProc = $AllProc[0]}
+else{$SelProc = $AllProc}
+if ($SelProc) {
+    Add-Type -AssemblyName System.Windows.Forms
+    $handle = $SelProc.MainWindowHandle
+    [void][User32]::SendMessage($handle, 0x0010, 0, 0)
+    #If the window is still open after the delay, just knock out the process entirely
+    Start-Sleep -Seconds $delaySeconds
+    $ChkProc = Get-Process | Where-Object {$_.MainWindowTitle -like $winttl}
+    $ChkTerm = $ChkProc | Select-Object Id, ProcessName
+    if ( -not (Compare-Object $PreTerm.PSObject.Properties $ChkTerm.PSObject.Properties) )
+    {
+        Stop-Process -ID $SelProc.ID -Force
+    }
+    
+}
