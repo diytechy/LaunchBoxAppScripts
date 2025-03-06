6298fe86528934f249c56a71c60cd2b857b0be48
Peter Johnson <peterandersjohnson@outlook.com>
2025-03-05 23:24:00 -0600

    Drafting out continuously running check, to execute different overlays depending on what is happening.

--- ScreenOverlayOnProperty.ps1
+++ ScreenOverlayOnProperty.ps1
@@ -7,6 +7,10 @@
 '@
 $s_prev = 0
 
+
+$stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+$LastMovement = $StopWatch.Elapsed.TotalSeconds
+$OverlayMode = 0
 #Idle loop check, only applies when 
 While ($True) {
 	$ksum = 0
@@ -16,12 +20,27 @@ While ($True) {
 			$ksum = $ksum + $k
 		}
 	}
+    #Get active window (Maybe don't need to do this so often?
+
     $mp = $position = [System.Windows.Forms.Cursor]::Position
 	$s = $ksum
+    $WaitTime = $StopWatch.Elapsed.TotalSeconds-$LastMovement
 	If (($s_prev -ne $s) -or ($mp_prev -ne $mp)) {
-		Write-Host $ksum
+		#Write-Host $ksum
 		$s_prev = $s
         $mp_prev = $mp
+        $LastMovement = $StopWatch.Elapsed.TotalSeconds
+        #If the active window is for a dual save slot game emulator, overlay.
+            #Else make sure the overlay is closed.
+        #If the active window is for windows and the button is pressed, overlay?
+            #Else make sure the overlay is closed.
 	}
+    #Do stuff when idle for "x" period.
+    else
+    {
+        Write-Host $WaitTime
+    }
+    #If the active window is music related and the time has expired, open ProjectM
+        #Else make sure the overlay is closed.
 	Start-Sleep -Milliseconds 50
 }
