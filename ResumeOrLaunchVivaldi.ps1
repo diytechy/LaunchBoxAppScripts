b1261b7c9894f065f16a8e4a5f98452649fcfac3
Peter Johnson <peterandersjohnson@outlook.com>
2025-03-09 17:40:15 -0500

    Vivaldi launcher script, since it is impossible to jump to specific URL.  Which... is annoying.

---
+++ ResumeOrLaunchVivaldi.ps1
@@ -0,0 +1,20 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+Add-Type -TypeDefinition @'
+	using System;
+	using System.Runtime.InteropServices;
+    using System.Text;
+	public class User32{
+        [DllImport("user32.dll")] [return: MarshalAs(UnmanagedType.Bool)]  public static extern bool SetForegroundWindow(IntPtr hWnd);
+	}
+'@
+ 
+$IDHndl = @(get-process | ? { $_.MainWindowTitle -match "vivaldi"})
+#If the process is open, switch to it.
+if($IDHndl){
+    [User32]::SetForegroundWindow($IDHndl[0].mainwindowhandle)
+}
+#Else, open it.
+else{
+    Start-Process -FilePath "vivaldi"
+}
