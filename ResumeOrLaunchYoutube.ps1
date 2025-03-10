e92f43e2244dfba9aa642f8ac7c0ebf6a0d44208
Peter Johnson <peterandersjohnson@outlook.com>
2025-03-10 16:13:23 -0500

    Comitting scripts for reference, may remove.

---
+++ ResumeOrLaunchYoutube.ps1
@@ -0,0 +1,26 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+Add-Type -TypeDefinition @'
+	using System;
+	using System.Runtime.InteropServices;
+    using System.Text;
+	public class User32{
+        [DllImport("user32.dll")] [return: MarshalAs(UnmanagedType.Bool)]  public static extern bool SetForegroundWindow(IntPtr hWnd);
+	}
+'@
+ 
+$IDHndl = @(get-process | ? { $_.MainWindowTitle -match "youtube"})
+#$IDHndl = @(get-process | ? { $_.ProcessName -match "msedge"})
+$IDHndl = @(get-process | ? { $_.ProcessName -match "vivaldi"})
+#If the process is open, switch to it.
+if($IDHndl){
+    [User32]::SetForegroundWindow($IDHndl[0].mainwindowhandle)
+}
+#Else, open it.
+else{
+    #Start-Process -FilePath "msedge" -ArgumentList "--start-maximized", "https://www.youtube.com/"  -WindowStyle Normal
+    #Start-Process -FilePath "msedge" -WindowStyle ([System.Diagnostics.ProcessWindowStyle]::Maximized), "https://www.youtube.com/"
+    Start-Process -FilePath "msedge" -ArgumentList "--new-window", "--start-fullscreen", "https://www.youtube.com/"
+    Start-Process -FilePath "vivaldi" -ArgumentList "--new-window", "--start-fullscreen", "https://www.youtube.com/"
+    Start-Process -FilePath "vivaldi" -ArgumentList "https://www.youtube.com/"
+}
