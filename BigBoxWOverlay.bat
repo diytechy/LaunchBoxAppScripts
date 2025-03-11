e273fc3798367a0241c0f3c64c5f5404de2df947
Peter Johnson <peterandersjohnson@outlook.com>
2025-03-11 14:50:01 -0500

    Launching with overlay functional

--- BigBoxWOverlay.bat
+++ BigBoxWOverlay.bat
@@ -3,12 +3,12 @@ setlocal
 
 :: Define a relative path
 set relativePath=..\LaunchBox\LaunchBox.exe
-
+set overlayPS=ScreenOverlayOnProperty.ps1
 :: Combine script's path with the relative path
-set fullPath=%~dp0%relativePath%
-
-echo Full Path: %fullPath%
-call %fullPath%
-
+set LBPath=%~dp0%relativePath%
+set OLPath=%~dp0%overlayPS%
+::echo Full Path: %fullPath%
+call %LBPath%
+powershell.exe -noexit -WindowStyle Minimized -file "%OLPath%"
 endlocal
 pause
